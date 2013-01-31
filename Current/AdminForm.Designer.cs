bf2d2e71e1b8533cf3d4bca787fb4d3310a802bc
Benjamin Buckmaster <bpbuckmaster@coe.edu>
2013-01-31 00:26:19 -0600

    Much admin functionality.
    Student Schedule and Advisor can now be changed by admin.

--- Current/AdminForm.Designer.cs
+++ Current/AdminForm.Designer.cs
@@ -31,14 +31,25 @@
             this.logoutBtn = new System.Windows.Forms.Button();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.button1 = new System.Windows.Forms.Button();
+            this.StudentButton = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.FastRegButton = new System.Windows.Forms.Button();
+            this.AdvisorButton = new System.Windows.Forms.Button();
+            this.DetailBox = new System.Windows.Forms.TextBox();
+            this.YesButton = new System.Windows.Forms.Button();
+            this.NoButton = new System.Windows.Forms.Button();
+            this.ActualRegButton = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
             this.SuspendLayout();
             // 
             // logoutBtn
             // 
-            this.logoutBtn.Location = new System.Drawing.Point(544, 211);
+            this.logoutBtn.Location = new System.Drawing.Point(578, 409);
             this.logoutBtn.Name = "logoutBtn";
-            this.logoutBtn.Size = new System.Drawing.Size(131, 23);
+            this.logoutBtn.Size = new System.Drawing.Size(97, 23);
             this.logoutBtn.TabIndex = 0;
             this.logoutBtn.Text = "Logout";
             this.logoutBtn.UseVisualStyleBackColor = true;
@@ -47,36 +58,142 @@
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Location = new System.Drawing.Point(12, 164);
             this.dataGridView1.MultiSelect = false;
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
-            this.dataGridView1.Size = new System.Drawing.Size(663, 193);
+            this.dataGridView1.Size = new System.Drawing.Size(320, 239);
             this.dataGridView1.TabIndex = 1;
-            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(13, 213);
+            this.button1.Location = new System.Drawing.Point(12, 409);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(117, 20);
+            this.button1.Size = new System.Drawing.Size(197, 23);
             this.button1.TabIndex = 2;
-            this.button1.Text = "View as...";
+            this.button1.Text = "View Courses";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // StudentButton
+            // 
+            this.StudentButton.Location = new System.Drawing.Point(12, 12);
+            this.StudentButton.Name = "StudentButton";
+            this.StudentButton.Size = new System.Drawing.Size(209, 146);
+            this.StudentButton.TabIndex = 3;
+            this.StudentButton.Text = "Modify Student";
+            this.StudentButton.UseVisualStyleBackColor = true;
+            this.StudentButton.Click += new System.EventHandler(this.ViewStudents);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(227, 12);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(206, 146);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Modify Faculty";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(439, 12);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(236, 146);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "Modify Course";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(339, 164);
+            this.dataGridView2.MultiSelect = false;
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView2.Size = new System.Drawing.Size(335, 238);
+            this.dataGridView2.TabIndex = 6;
+            // 
+            // FastRegButton
+            // 
+            this.FastRegButton.Location = new System.Drawing.Point(339, 409);
+            this.FastRegButton.Name = "FastRegButton";
+            this.FastRegButton.Size = new System.Drawing.Size(123, 23);
+            this.FastRegButton.TabIndex = 7;
+            this.FastRegButton.Text = "Add Student to Course";
+            this.FastRegButton.UseVisualStyleBackColor = true;
+            this.FastRegButton.Click += new System.EventHandler(this.FastRegButton_Click);
+            // 
+            // AdvisorButton
+            // 
+            this.AdvisorButton.Location = new System.Drawing.Point(215, 409);
+            this.AdvisorButton.Name = "AdvisorButton";
+            this.AdvisorButton.Size = new System.Drawing.Size(117, 23);
+            this.AdvisorButton.TabIndex = 8;
+            this.AdvisorButton.Text = "View Advisor";
+            this.AdvisorButton.UseVisualStyleBackColor = true;
+            this.AdvisorButton.Click += new System.EventHandler(this.AdvisorButton_Click);
+            // 
+            // DetailBox
+            // 
+            this.DetailBox.Location = new System.Drawing.Point(385, 197);
+            this.DetailBox.Name = "DetailBox";
+            this.DetailBox.Size = new System.Drawing.Size(241, 20);
+            this.DetailBox.TabIndex = 9;
+            // 
+            // YesButton
+            // 
+            this.YesButton.Location = new System.Drawing.Point(385, 223);
+            this.YesButton.Name = "YesButton";
+            this.YesButton.Size = new System.Drawing.Size(116, 26);
+            this.YesButton.TabIndex = 10;
+            this.YesButton.Text = "Yes";
+            this.YesButton.UseVisualStyleBackColor = true;
+            this.YesButton.Click += new System.EventHandler(this.YesButton_Click);
+            // 
+            // NoButton
+            // 
+            this.NoButton.Location = new System.Drawing.Point(507, 223);
+            this.NoButton.Name = "NoButton";
+            this.NoButton.Size = new System.Drawing.Size(119, 26);
+            this.NoButton.TabIndex = 11;
+            this.NoButton.Text = "No";
+            this.NoButton.UseVisualStyleBackColor = true;
+            this.NoButton.Click += new System.EventHandler(this.NoButton_Click);
+            // 
+            // ActualRegButton
+            // 
+            this.ActualRegButton.Location = new System.Drawing.Point(466, 409);
+            this.ActualRegButton.Name = "ActualRegButton";
+            this.ActualRegButton.Size = new System.Drawing.Size(112, 23);
+            this.ActualRegButton.TabIndex = 12;
+            this.ActualRegButton.Text = "Register";
+            this.ActualRegButton.UseVisualStyleBackColor = true;
+            this.ActualRegButton.Click += new System.EventHandler(this.ActualRegButton_Click);
             // 
             // AdminForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(687, 246);
+            this.ClientSize = new System.Drawing.Size(685, 441);
+            this.Controls.Add(this.ActualRegButton);
+            this.Controls.Add(this.NoButton);
+            this.Controls.Add(this.YesButton);
+            this.Controls.Add(this.DetailBox);
+            this.Controls.Add(this.AdvisorButton);
+            this.Controls.Add(this.FastRegButton);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.StudentButton);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.logoutBtn);
             this.Name = "AdminForm";
             this.Text = "AdminForm";
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -85,5 +202,15 @@
         private System.Windows.Forms.Button logoutBtn;
         private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button StudentButton;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.Button FastRegButton;
+        private System.Windows.Forms.Button AdvisorButton;
+        private System.Windows.Forms.TextBox DetailBox;
+        private System.Windows.Forms.Button YesButton;
+        private System.Windows.Forms.Button NoButton;
+        private System.Windows.Forms.Button ActualRegButton;
     }
 }
--- Current/AdminForm.cs
+++ Current/AdminForm.cs
@@ -13,85 +13,204 @@ namespace BensCRS
     {
         UserAdmin admin;
         private UserStudent asthisstudent;
+        private UserFaculty asthisfaculty;
+        private Course thisCourse; 
         List<UserFaculty> FacultyList = new List<UserFaculty>();
         List<UserStudent> StudentList = new List<UserStudent>();
         List<Course> Courses = new List<Course>();
-        private adminstate state; 
+        private adminstate state;
+        private studentstate studstate;
 
         private enum adminstate 
         { 
-            asadmin,
-            asfaculty,
-            asstudent 
+            dflt,
+            student,
+            faculty
+        }
+
+        private enum studentstate
+        { 
+            sviewall,
+            ssched,
+            salcourses,
+            sadvisor
         }
 
         public AdminForm(List<UserFaculty> fl, List<UserStudent> sl, List<Course> cl, UserAdmin adm)
         {
-            state = adminstate.asadmin; 
+            state = adminstate.dflt; 
             InitializeComponent();
             admin = adm;
             FacultyList = fl;
             StudentList = sl;
             Courses = cl;
-            initdgv(); 
+            hidethings(); 
             
         }
 
-        public AdminForm(List<UserFaculty> fl, List<UserStudent> sl, List<Course> cl, UserAdmin adm, UserStudent std)
+        private void hidethings()
         {
-            InitializeComponent();
-            state = adminstate.asstudent;
-            asthisstudent = std;
-            button1.Text = "Add " + asthisstudent.UserName + " to Course.";
-            logoutBtn.Text = "Logout as " + asthisstudent.UserName; 
-            admin = adm;
-            FacultyList = fl;
-            StudentList = sl;
-            Courses = cl;
-            initdgv(); 
-            
-        
-        }
-        private void initdgv()
-        {
-            if (state == adminstate.asadmin)
-                dataGridView1.DataSource = StudentList; 
-            if (state == adminstate.asstudent)
-                dataGridView1.DataSource = Courses;                 
+            button1.Hide();
+            AdvisorButton.Hide();
+            dataGridView2.Hide();
+            FastRegButton.Hide();
+            ActualRegButton.Hide(); 
+            hideDetail(); 
         }
 
         private void logoutBtn_Click(object sender, EventArgs e)
         {
-            if (state == adminstate.asadmin)
                 this.Close();
-            if (state == adminstate.asstudent)
-            {
-                AdminForm nextform = new AdminForm(FacultyList, StudentList, Courses, admin);
-                nextform.Show();
-                this.Close(); 
-            }
         }
 
-        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
-        {
-
-        }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            if (state == adminstate.asadmin)
+
+            switch (state)
             {
-                AdminForm anext = new AdminForm(FacultyList, StudentList, Courses, admin, StudentList[dataGridView1.SelectedRows[0].Index]);
-                anext.Show();
-                this.Close(); 
+
+                case adminstate.dflt: { } break; //Do nothing.
+                case adminstate.student:
+                    if (studstate == studentstate.sviewall)
+                    {
+                        asthisstudent = StudentList[dataGridView1.SelectedRows[0].Index];
+                        //MessageBox.Show("Student : " + asthisstudent.FirstName + " " + asthisstudent.LastName); 
+                        studstate = studentstate.ssched;
+                        this.Text = "View/Change " + asthisstudent.UserName + "'s course schedule.";
+                        List<Course> StdCrs = new List<Course>();
+                        foreach (Course C in Courses)
+                        {
+                            if (asthisstudent.MyCourses.Contains(C.CourseName))
+                            {
+                                StdCrs.Add(C);
+                            }
+                        }
+                        dataGridView1.DataSource = StdCrs;
+                        AdvisorButton.Hide(); 
+                        button1.Text = "Student List";
+                        FastRegButton.Text = "View all courses";
+                        FastRegButton.Show();
+                    }
+                    else
+                    {
+                        //state = adminstate.dflt;
+                        //AdvisorButton.Show();
+                        //MessageBox.Show("State 2b");
+                        IVS(); 
+
+                    }
+                    break;
+                default: //MessageBox.Show("CASE 3");
+                                        break;
             }
-            if (state == adminstate.asstudent)
+        }
+
+        private void ViewStudents(object sender, EventArgs e)
+        {
+            IVS();             
+        }
+
+        private void IVS() //"Inner View Students." I wanted to call it without event arguments.
+        {
+            this.Text = "Viewing List of Students"; 
+            state = adminstate.student;
+            studstate = studentstate.sviewall;
+            dataGridView1.DataSource = StudentList;
+            FastRegButton.Hide(); 
+            button1.Text = "View Student Courses";
+            button1.Show();
+            AdvisorButton.Show();
+        }
+
+
+        private void AdvisorButton_Click(object sender, EventArgs e)
+        {
+            studstate = studentstate.sadvisor;
+            asthisstudent = StudentList[dataGridView1.SelectedRows[0].Index];
+            this.Text = "View/Change " + asthisstudent.UserName + "'s advisor.";
+            StringBuilder OutString = new StringBuilder();
+            OutString.Append(asthisstudent.UserName);
+            OutString.Append("'s advisor is ");
+            OutString.Append(asthisstudent.Advisor);
+            OutString.Append(". Change?");
+            DetailBox.Text = OutString.ToString(); 
+            showDetail(); 
+        }
+
+        private void showDetail()
+        {
+            DetailBox.Show();
+            YesButton.Show();
+            NoButton.Show();
+        }
+
+        private void hideDetail()
+        {
+            DetailBox.Hide();
+            YesButton.Hide();
+            NoButton.Hide();
+        }
+
+        private void FastRegButton_Click(object sender, EventArgs e)
+        {
+            if (studstate == studentstate.ssched)
             {
-                benutil.AddStudenttoCourse(asthisstudent, Courses[dataGridView1.SelectedRows[0].Index], admin);
-                initdgv(); 
+                studstate = studentstate.salcourses;
+                FastRegButton.Text = "Cancel";
+                dataGridView2.DataSource = Courses;
+                dataGridView2.Show();
+                ActualRegButton.Show(); 
+            }
+            else
+            {
+                if (studstate == studentstate.salcourses)
+                {
+                    studstate = studentstate.ssched;
+                    dataGridView2.Hide();
+                    ActualRegButton.Hide(); 
+
+                    FastRegButton.Text = "Add Student to Course";
+                }
+            }
+        }
+
+        private void ActualRegButton_Click(object sender, EventArgs e)
+        {
+            if (studstate == studentstate.salcourses)
+            {
+                thisCourse = Courses[dataGridView2.SelectedRows[0].Index];
+                benutil.AddStudenttoCourse(asthisstudent, thisCourse, admin);
             }
 
+            if (studstate == studentstate.sadvisor) 
+            {
+                asthisstudent.Advisor = FacultyList[dataGridView2.SelectedRows[0].Index].UserName;
+                ActualRegButton.Hide();
+                dataGridView2.Hide(); 
+                IVS(); 
+            }
         }
+
+        private void YesButton_Click(object sender, EventArgs e)
+        {
+            hideDetail();
+            dataGridView2.DataSource = FacultyList;
+            dataGridView2.Show();
+            ActualRegButton.Text = "Set Advisor";
+            ActualRegButton.Show(); 
+        }
+
+        private void NoButton_Click(object sender, EventArgs e)
+        {
+            hideDetail();
+            IVS(); 
+        }
+
+
+
+
+
 
 
 
